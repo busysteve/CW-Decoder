8cddef1127ed3b9c4964f78a17dcaf0710db6930
Stephen Mathews <busysteve@gmail.com>
2025-01-14 11:50:57 -0500

    Update cw_quizzer.ino

--- cw_quizzer.ino
+++ cw_quizzer.ino
@@ -809,7 +809,7 @@ void ditcalc() {
   dahtime    = (DITCONST * 3)/keyerwpm;
   lettergap1 = (DITCONST * 2.5)/(keyerwpm);
   lettergap2   = (DITCONST * 3)/(farn);
-  wordgap1   = (DITCONST * 5)/(keyerwpm);
+  wordgap1   = (DITCONST * 4.5)/(keyerwpm);
   wordgap2   = (DITCONST * 7)/(farn);
 }
 
